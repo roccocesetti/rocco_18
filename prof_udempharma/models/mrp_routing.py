490a87b0c582f17623344ce599d99f49634f61ae
roccocesetti <r.cesetti@gmail.com>
2025-09-20 10:29:45 +0200

    ok

--- prof_udempharma/models/mrp_routing.py
+++ prof_udempharma/models/mrp_routing.py
@@ -9,34 +9,14 @@ from odoo.tools import float_round
 class MrpRoutingWorkcenter(models.Model):
     _inherit = "mrp.routing.workcenter"
 
-    time_cycle_second = fields.Float('Secondi', compute="_compute_time_cycle")
-    @api.depends('time_cycle_manual', 'time_mode', 'workorder_ids')
-    def _compute_time_cycle(self):
-        manual_ops = self.filtered(lambda operation: operation.time_mode == 'manual')
-        for operation in manual_ops:
-            operation.time_cycle = operation.time_cycle_manual
-        for operation in self - manual_ops:
-            data = self.env['mrp.workorder'].search([
-                ('operation_id', 'in', operation.ids),
-                ('qty_produced', '>', 0),
-                ('state', '=', 'done')],
-                limit=operation.time_mode_batch,
-                order="date_finished desc, id desc")
-            # To compute the time_cycle, we can take the total duration of previous operations
-            # but for the quantity, we will take in consideration the qty_produced like if the capacity was 1.
-            # So producing 50 in 00:10 with capacity 2, for the time_cycle, we assume it is 25 in 00:10
-            # When recomputing the expected duration, the capacity is used again to divide the qty to produce
-            # so that if we need 50 with capacity 2, it will compute the expected of 25 which is 00:10
-            total_duration = 0  # Can be 0 since it's not an invalid duration for BoM
-            cycle_number = 0  # Never 0 unless infinite item['workcenter_id'].capacity
-            for item in data:
-                total_duration += item['duration']
-                capacity = item['workcenter_id']._get_capacity(item.product_id)
-                qty_produced = item.product_uom_id._compute_quantity(item['qty_produced'], item.product_id.uom_id)
-                cycle_number += float_round((qty_produced / capacity or 1.0), precision_digits=0, rounding_method='UP')
-            if cycle_number:
-                operation.time_cycle = total_duration / cycle_number
-            else:
-                operation.time_cycle = operation.time_cycle_manual
+     time_cycle_second = fields.Float(
+        string="Cycle Time (Seconds)",
+        compute="_compute_time_cycle_second",
+        store=True,
+    )
 
-            operation.time_cycle_second = operation.time_cycle * 60
+    @api.depends('time_cycle')
+    def _compute_time_cycle_second(self):
+        for rec in self:
+            rec.time_cycle_second = float(rec.time_cycle or 0.0) * 60.0
+
